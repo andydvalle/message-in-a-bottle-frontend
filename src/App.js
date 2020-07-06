84d322c70a3562b35e5ce45fe45eba0994119ed1
github username <github email address>
2020-07-06 13:16:30 -0700

    deletes unwanted files

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
 import NavBar from './components/NavBar.js'
+import Dashboard from './containers/Dashboard.js'
+import Inbox from './containers/Inbox.js'
+import Journal from './containers/Journal.js'
 
 class App extends Component {
   render(){
@@ -9,6 +12,9 @@ class App extends Component {
     <div>
         Hi from app
         <NavBar />
+        <Dashboard />
+        <Inbox />
+        <Journal />
     </div>
     );
   }
