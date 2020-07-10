ff93e59759637c7798d9eb272d4edb7dd692a9e2
github username <github email address>
2020-07-09 20:08:16 -0700

    adds jumbotron

--- src/components/Login.js
+++ src/components/Login.js
@@ -36,11 +36,29 @@ class Login extends React.Component {
     });
   };
 
- 
   render() {
     const { fields } = this.state;
     return (
       <div className="container mt-5">
+        <div class="jumbotron">
+          <div className="jumbotron-content col-sm-4">
+            <h1 class="display-4">Hello, world!</h1>
+            <p class="lead">
+              This is a simple hero unit, a simple jumbotron-style component for
+              calling extra attention to featured content or information.
+            </p>
+            <hr class="my-4" />
+            <p>
+              It uses utility classes for typography and spacing to space
+              content out within the larger container.
+            </p>
+            <p class="lead">
+              <a class="btn btn-primary btn-lg" href="#" role="button">
+                Learn more
+              </a>
+            </p>
+          </div>
+        </div>
         <div class="card col-sm-6">
           {this.state.error ? <h1>Try Again</h1> : null}
           <div class="card-body">
